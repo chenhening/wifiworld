69734d94590a3cc3496cac16a3abe1d5155e1467
buffer8848 <179770346@qq.com>
2015-01-24 01:32:44 +0800

    add location feature

--- client/android/WifiWorld/src/com/anynet/wifiworld/ui/map/MapFragment.java
+++ client/android/WifiWorld/src/com/anynet/wifiworld/ui/map/MapFragment.java
@@ -7,22 +7,38 @@
  */
 package com.anynet.wifiworld.ui.map;
 
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.MapView;
 import com.anynet.wifiworld.R;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class MapFragment extends Fragment {
-	private MapView mapView;
-	private AMap aMap;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+
+public class MapFragment 
+	extends Fragment implements LocationSource, AMapLocationListener, OnCheckedChangeListener {
 	private View mapLayout;
 	
+	private MapView mapView;
+	private AMap aMap;
+	private OnLocationChangedListener mListener;
+	private LocationManagerProxy mAMapLocationManager;
+	
+	//---------------------------------------------------------------------------------------------
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +46,13 @@ public class MapFragment extends Fragment {
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		mapLayout = inflater.inflate(R.layout.basicmap_activity, null);
+		mapLayout = inflater.inflate(R.layout.activity_multy_location, null);
 		mapView = (MapView) mapLayout.findViewById(R.id.map);
 		mapView.onCreate(savedInstanceState);
 		if (aMap == null) {
 			aMap = mapView.getMap();
 		}
+		setUpMap();
 		
         return mapLayout;
     }
@@ -44,12 +61,12 @@ public class MapFragment extends Fragment {
     public void onPause() {
         super.onPause();
         mapView.onPause();
+        deactivate();
     }
 	
 	@Override
     public void onStop() {
         super.onStop();
-        System.out.println("Fragment-->onStop");
     }
 	
 	@Override
@@ -74,4 +91,95 @@ public class MapFragment extends Fragment {
         super.onDestroy();
         mapView.onDestroy();
     }
+
+	//---------------------------------------------------------------------------------------------
+	/**
+	 * 激活定位
+	 */
+	@Override
+	public void activate(OnLocationChangedListener listener) {
+		mListener = listener;
+		if (mAMapLocationManager == null) {
+			mAMapLocationManager = LocationManagerProxy.getInstance(this.getActivity());
+			// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+			// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），
+			// 并且在合适时间调用removeUpdates()方法来取消定位请求
+			// 在定位结束后，在合适的生命周期调用destroy()方法
+			// 其中如果间隔时间为-1，则定位只定一次
+			// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求
+			mAMapLocationManager.requestLocationData(
+					LocationProviderProxy.AMapNetwork, 60 * 1000, 10, this);
+		}
+	}
+
+	/**
+	 * 停止定位
+	 */
+	@Override
+	public void deactivate() {
+		mListener = null;
+		if (mAMapLocationManager != null) {
+			mAMapLocationManager.removeUpdates(this);
+			mAMapLocationManager.destroy();
+		}
+		mAMapLocationManager = null;
+	}
+	
+	//---------------------------------------------------------------------------------------------
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 定位成功后回调函数
+	 */
+	@Override
+	public void onLocationChanged(AMapLocation amapLocation) {
+		if (mListener != null && amapLocation != null) {
+			if (amapLocation != null && amapLocation.getAMapException().getErrorCode() == 0) {
+				mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+			} else {
+				Log.e("AmapErr","Location ERR:" + amapLocation.getAMapException().getErrorCode());
+			}
+		}
+	}
+	
+	//---------------------------------------------------------------------------------------------
+	/**
+	 * 设置一些amap的属性
+	 */
+	private void setUpMap() {
+		aMap.setLocationSource(this);// 设置定位监听
+		aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+		// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+		aMap.setMyLocationEnabled(true);
+		// 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+		aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_FOLLOW);
+	}
 }
