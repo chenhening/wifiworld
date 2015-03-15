ba89f9cbf181e31e13cc8556cdcd7734120edf4d
csdnnet <1398683@163.com>
2015-03-15 11:43:30 +0800

    Modfiy for wifi status of provider

--- client/android/WifiWorldBaby/src/com/anynet/wifiworld/app/BaseActivity.java
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/app/BaseActivity.java
@@ -328,7 +328,6 @@ public class BaseActivity extends FragmentActivity
 	@Override
 	public void onBackPressed() {
 		// TODO Auto-generated method stub
-		Toast.makeText(getApplicationContext(), "accccccccccccccc", Toast.LENGTH_LONG).show();
 		super.onBackPressed();
 	}
     
