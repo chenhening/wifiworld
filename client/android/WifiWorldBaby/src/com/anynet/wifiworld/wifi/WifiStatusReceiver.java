fd7414fba0e7cc875a95166ae39b375a01c7b188
Chen, Jiansheng <jianshengx.chen@intel.com>
2015-03-29 11:56:54 +0800

    active wifi status display

--- client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/WifiStatusReceiver.java
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/WifiStatusReceiver.java
@@ -5,11 +5,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.net.NetworkInfo;
 import android.net.NetworkInfo.State;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.os.Parcelable;
 import android.util.Log;
@@ -21,9 +23,15 @@ public class WifiStatusReceiver {
 	public static void schedule(final Context ctx) {
 		ctx.startService(new Intent(ctx, WifiMonitorService.class));
 	}
+	
+	public static void bindWifiService(final Context ctx, ServiceConnection conn) {
+		ctx.bindService(new Intent(ctx, WifiMonitorService.class), conn, Context.BIND_AUTO_CREATE);
+	}
 
 	public static class WifiMonitorService extends Service {
 		private IntentFilter mIntentFilter;
+		private OnWifiStatusListener onWifiStatusListener;
+		private String statusStr;
 		
 		private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
 
@@ -32,57 +40,63 @@ public class WifiStatusReceiver {
 				String action = intent.getAction();
 		        if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)) {
 		        	Log.i(TAG, "network state changed action");
-		           Parcelable parcelableExtra = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-		           if (null != parcelableExtra) {
-		               NetworkInfo networkInfo = (NetworkInfo) parcelableExtra;
-		               State state = networkInfo.getState();
-		               boolean isConnected = state==State.CONNECTED;
-		               if(isConnected){
-		                     Toast.makeText(context, "Wifi已经连接", Toast.LENGTH_LONG).show();
-		               }
-		           }    
-		        } 
+		        	Parcelable parcelableExtra = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+		        	if (null != parcelableExtra) {
+		        		NetworkInfo networkInfo = (NetworkInfo) parcelableExtra;
+		        		State state = networkInfo.getState();
+		        		boolean isConnected = state==State.CONNECTED;
+		        		if(isConnected){
+		        			statusStr = "已连接" + WifiAdmin.convertToNonQuotedString(WifiAdmin.getInstance(context).getWifiNameConnection());
+		        			if (onWifiStatusListener != null) {
+				            	onWifiStatusListener.onChanged(statusStr);
+		        			}
+		        			Toast.makeText(context, statusStr, Toast.LENGTH_SHORT).show();
+		        		}
+		        	}
+		        }
 		        if (WifiManager.SUPPLICANT_STATE_CHANGED_ACTION.equals(action)) {
 		        	Log.i(TAG, "supplicant state changed action");
 		            WifiInfo info = WifiAdmin.getInstance(context).getWifiConnection();
 		            SupplicantState state = info.getSupplicantState();
-		            String str = "";
 		            if (state == SupplicantState.ASSOCIATED){
-		                str = "正在连接...";
-		            } 
+		                statusStr = "正在连接...";
+		            }
 		            //为了兼容4.0以下的设备，不要写成state == SupplicantState.AUTHENTICATING
 		            else if(state.toString().equals("AUTHENTICATING")){
-		                str = "正在验证";
+		                statusStr = "正在验证";
 		            }
 		            else if (state == SupplicantState.ASSOCIATING){
-		                str = "正在连接...";
+		                statusStr = "正在连接...";
 		            } else if (state == SupplicantState.COMPLETED){
 		                //只是验证密码正确，并不代表连接成功
-		                str = "正在获取ip地址"; 
+		                statusStr = "正在获取ip地址";
 		            } else if (state == SupplicantState.DISCONNECTED){
-		                str = "已断开"; 
+		                statusStr = "已断开";
 		            } else if (state == SupplicantState.DORMANT){
-		                str = "暂停活动";
+		                statusStr = "暂停活动";
 		            } else if (state == SupplicantState.FOUR_WAY_HANDSHAKE){
-		                str = "正在连接..."; 
+		                statusStr = "正在连接...";
 		            } else if (state == SupplicantState.GROUP_HANDSHAKE){
-		                str = "正在连接...";
+		                statusStr = "正在连接...";
 		            } else if (state == SupplicantState.INACTIVE){
-		                str = "已断开";
+		                statusStr = "已断开";
 		            } else if (state == SupplicantState.INVALID){
-		                str = "无效";
+		                statusStr = "无效";
 		            } else if (state == SupplicantState.SCANNING){
-		                str = "正在扫描...";
+		                statusStr = "正在扫描...";
 		            } else if (state == SupplicantState.UNINITIALIZED){
-		                str = "未初始化";
+		                statusStr = "未初始化";
 		            }else{
-		                str = "unkown";
+		                statusStr = "unkown";
 		            }
 		            final int errorCode = intent.getIntExtra(WifiManager.EXTRA_SUPPLICANT_ERROR, -1);
 		            if (errorCode == WifiManager.ERROR_AUTHENTICATING) {
 		                //密码错误
 		            }
-		            Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+		            if (onWifiStatusListener != null) {
+		            	onWifiStatusListener.onChanged(statusStr);
+					}
+		            //Toast.makeText(context, statusStr, Toast.LENGTH_SHORT).show();
 		        }
 		        
 			}
@@ -92,13 +106,14 @@ public class WifiStatusReceiver {
 		@Override
 		public IBinder onBind(Intent intent) {
 			// TODO Auto-generated method stub
-			return null;
+			return new WifiStatusBinder();
 		}
 
 		@Override
 		public void onCreate() {
 			super.onCreate();
 			Log.i(TAG, "Create broadcast service");
+			mIntentFilter = new IntentFilter();
 			mIntentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
 			mIntentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
 			registerReceiver(mBroadcastReceiver, mIntentFilter);
@@ -110,6 +125,19 @@ public class WifiStatusReceiver {
 			unregisterReceiver(mBroadcastReceiver);
 		}
 		
+		public class WifiStatusBinder extends Binder {
+			public WifiMonitorService getService() {
+				return WifiMonitorService.this;
+			}
+		}
+		
+		public void setOnWifiStatusListener(OnWifiStatusListener onWifiStatusListener) {
+			this.onWifiStatusListener = onWifiStatusListener;
+		}
+		
 	}
 	
+	public interface OnWifiStatusListener {
+		void onChanged(String str);
+	}
 }
