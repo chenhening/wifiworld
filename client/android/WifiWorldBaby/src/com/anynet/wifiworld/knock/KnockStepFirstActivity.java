d60c6861af711a42d4f6ccc0bd0b56fc9d63dbd0
csdnnet <1398683@163.com>
2015-04-07 21:05:23 +0800

    add step fragment

---
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/knock/KnockStepFirstActivity.java
@@ -0,0 +1,74 @@
+package com.anynet.wifiworld.knock;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.anynet.wifiworld.R;
+import com.anynet.wifiworld.SettingFeedbackActivity;
+import com.anynet.wifiworld.app.BaseActivity;
+
+public class KnockStepFirstActivity extends BaseActivity {
+
+	StepFragment mSetupFragment[];
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+
+		setContentView(R.layout.knock_step_setup);
+		super.onCreate(savedInstanceState);
+		initView();
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		mSetupFragment = new StepFragment[3];
+		for(int i=0;i<3;i++){
+			mSetupFragment[i]= new StepFragment();
+		}
+		// fragments = new MainFragment[] { wifiFragment, mapFragment,
+		// discoverFragment, meFragment };
+		// 添加显示第一个fragment
+		getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mSetupFragment[0])
+				.add(R.id.fragment_container, mSetupFragment[1]).add(R.id.fragment_container, mSetupFragment[2])
+				.hide(mSetupFragment[1]).hide(mSetupFragment[2]).show(mSetupFragment[0]).commit();
+		setTitleUI();
+	}
+
+	public void setTitleUI() {
+		// TODO Auto-generated method stub
+		mTitlebar.ivHeaderLeft.setVisibility(View.INVISIBLE);
+		mTitlebar.llFinish.setVisibility(View.VISIBLE);
+		mTitlebar.llHeaderMy.setVisibility(View.INVISIBLE);
+		mTitlebar.tvHeaderRight.setVisibility(View.VISIBLE);
+		mTitlebar.tvHeaderRight.setText(getString(R.string.my));
+
+	}
+
+	public void next() {
+
+	}
+
+	public void pre() {
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+}
---
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/knock/KnockTopActivity.java
@@ -0,0 +1,114 @@
+package com.anynet.wifiworld.knock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+
+import com.anynet.wifiworld.R;
+import com.anynet.wifiworld.app.BaseActivity;
+public class KnockTopActivity extends BaseActivity {
+
+	private void bingdingTitleUI() {
+		mTitlebar.ivHeaderLeft.setVisibility(View.VISIBLE);
+		mTitlebar.llFinish.setVisibility(View.VISIBLE);
+		mTitlebar.llHeaderMy.setVisibility(View.INVISIBLE);
+		mTitlebar.tvHeaderRight.setVisibility(View.INVISIBLE);
+		mTitlebar.tvHeaderRight.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 如果第一页有未填写字段，则提示填写
+
+				// Intent i = null;
+				// i = new Intent(WifiProviderRigisterFirstActivity.this,
+				// WifiProviderRigisterSecondHomeActivity.class);
+				//
+				// startActivity(i);
+			}
+		});
+		mTitlebar.tvTitle.setText(getString(R.string.open_door));
+		mTitlebar.ivHeaderLeft.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+
+		setContentView(R.layout.knock_setting);
+		super.onCreate(savedInstanceState);
+		bingdingTitleUI();
+
+		RelativeLayout switchRL = (RelativeLayout) findViewById(R.id.tv_setting_knock);
+		switchRL.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				CheckBox ksCB = (CheckBox) findViewById(R.id.knock_switch);
+				if (ksCB.isChecked()) {
+					ksCB.setChecked(false);
+					findViewById(R.id.specific_setting_layout).setVisibility(View.GONE);
+				} else {
+					ksCB.setChecked(true);
+					findViewById(R.id.specific_setting_layout).setVisibility(View.VISIBLE);
+					findViewById(R.id.question_setting).setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							// TODO Auto-generated method stub
+							Intent i = new Intent(KnockTopActivity.this, KnockStepFirstActivity.class);
+							startActivity(i);
+						}
+					});
+				}
+
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		super.onNewIntent(intent);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+}
