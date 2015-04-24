4bc62cafa34ea95e1c1f54f516cb0c2ff9e55b58
binfeilx <binfeix.li@intel.com>
2015-04-24 10:23:15 +0800

    update license text

--- client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/WifiFragment.java
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/WifiFragment.java
@@ -585,7 +585,7 @@ public class WifiFragment extends MainFragment {
 			
 			//shared ui
 			TextView mTVLinkLicense = (TextView)mWifiShareLayout.findViewById(R.id.tv_link_license);
-			final String sText = "认证即表明您同意我们的<br><a href=\"activity.special.scheme://127.0.0.1\">《网络宝商户服务协议》</a>";
+			final String sText = "认证即表明您同意我们的<br><a href=\"activity.special.scheme://127.0.0.1\">《网络宝服务协议》</a>";
 			mTVLinkLicense.setText(Html.fromHtml(sText));
 			mTVLinkLicense.setClickable(true);
 			mTVLinkLicense.setMovementMethod(LinkMovementMethod.getInstance());
