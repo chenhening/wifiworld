be65baef8eec9b4ca9f62efe422f6490271e2c9d
binfeilx <binfeix.li@intel.com>
2015-05-07 10:12:54 +0800

    fixed ui error

--- client/android/WifiWorldBaby/src/com/anynet/wifiworld/me/WifiUsedListActivity.java
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/me/WifiUsedListActivity.java
@@ -260,7 +260,7 @@ public class WifiUsedListActivity extends BaseActivity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			if (convertView == null) {
-				convertView = View.inflate(getApplicationContext(), R.layout.item_my_whitelist, null);
+				convertView = View.inflate(getApplicationContext(), R.layout.item_wifi_user_list, null);
 				new ViewHolder(convertView);
 			}
 			ViewHolder holder = (ViewHolder) convertView.getTag();
