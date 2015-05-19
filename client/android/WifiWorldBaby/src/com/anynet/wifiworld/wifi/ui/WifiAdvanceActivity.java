730f9dbcede2e68c5900696184be6261037b61a7
Chen, Jiansheng <jianshengx.chen@intel.com>
2015-05-19 22:16:17 +0800

    add router login dialog, safe option check animation

--- client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/ui/WifiAdvanceActivity.java
+++ client/android/WifiWorldBaby/src/com/anynet/wifiworld/wifi/ui/WifiAdvanceActivity.java
@@ -1,6 +1,7 @@
 package com.anynet.wifiworld.wifi.ui;
 
 import com.anynet.wifiworld.R;
+import com.anynet.wifiworld.util.HandlerUtil.StaticHandler;
 import com.anynet.wifiworld.wifi.WifiAdmin;
 import com.wolf.routermanager.bean.AllRouterInfoBean;
 import com.wolf.routermanager.common.RouterUtilFactory;
@@ -12,6 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,6 +30,8 @@ public class WifiAdvanceActivity extends Activity {
 	private TextView mWifiName;
 	private WifiAdmin mWifiAdmin;
 	
+	private static int checkIdx = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.wifi_advance_activity);
@@ -43,20 +47,30 @@ public class WifiAdvanceActivity extends Activity {
 		mWifiName = (TextView)findViewById(R.id.wifi_advance_name);
 		mWifiName.setText(WifiAdmin.convertToNonQuotedString(mWifiAdmin.getWifiNameConnection()));
 		
-		final ProgressBar progressBar = (ProgressBar)findViewById(R.id.encrypt_process);
-		final ImageView imageView = (ImageView)findViewById(R.id.encrypt_result);
+		final int[] progressBars = {R.id.encrypt_process, R.id.dns_process, R.id.arp_process, R.id.wps_process,
+				R.id.reported_process, R.id.firewall_process};
+		final int[] imageResults = {R.id.encrypt_result, R.id.dns_result, R.id.arp_result, R.id.wps_result,
+				R.id.reported_result, R.id.firewall_result};
 		final TextView textView = (TextView)findViewById(R.id.wifi_encrypt_text);
-		progressBar.setVisibility(View.VISIBLE);
-		Handler handler = new Handler();
-		handler.postDelayed(new Runnable() {
+		final Handler checkHandler = new Handler();
+		checkIdx = 0;
+		findViewById(progressBars[checkIdx]).setVisibility(View.VISIBLE);
+		Runnable checkRunnable = new Runnable() {
 			
 			@Override
 			public void run() {
-				progressBar.setVisibility(View.GONE);
-				imageView.setVisibility(View.VISIBLE);
-				textView.setText(textView.getText() + "(" + security + ")");
+				findViewById(progressBars[checkIdx]).setVisibility(View.GONE);
+				findViewById(imageResults[checkIdx]).setVisibility(View.VISIBLE);
+				if (checkIdx++ == 0) {
+					textView.setText(textView.getText() + "(" + security + ")");
+				} else if (checkIdx > 5) {
+					return;
+				}
+				findViewById(progressBars[checkIdx]).setVisibility(View.VISIBLE);
+				checkHandler.postDelayed(this, 1000);
 			}
-		}, 1000);
+		};
+		checkHandler.post(checkRunnable);
 		
 		setRouterBtnClickListner();
 	}
