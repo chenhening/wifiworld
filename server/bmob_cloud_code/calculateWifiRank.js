6eb71c7838b9072a633dbf32ae45d48b0b22f007
binfeilx <binfeix.li@intel.com>
2015-05-29 21:04:59 +0800

    update cloud code and design images

--- server/bmob_cloud_code/calculateWifiRank.js
+++ server/bmob_cloud_code/calculateWifiRank.js
@@ -22,49 +22,70 @@ function onRequest(request, response, modules) {
 			return;
 			
 		var objects = JSON.parse(data);
-		for (var cursor in objects) {
-		    var rows = objects[cursor];
-		    for (var row in rows) {
-		        if (!map_rank.contains(cursor.MacAddr))
-					map_rank[cursor.Macaddr] = 0;
-				if (cursor.Type == 0) {//好评
-					map_rank[cursor.Macaddr] += 5;
-				} else if (cursor.Type == 1) {
-					map_rank[cursor.Macaddr] -= 5;
-				}
-		    }
-		}
+        for (var cursor in objects) {
+            var rows = objects[cursor];
+            for (var row in rows) {
+                if (map_rank[rows[row].MacAddr] == null) {
+                    map_rank[rows[row].MacAddr] = 0;
+                }
+                if (rows[row].Type == 0) {//好评
+                    map_rank[rows[row].MacAddr] += 5;
+                } else if (rows[row].Type == 1) {
+                    map_rank[rows[row].MacAddr] -= 5;
+                } else {
+                    map_rank[rows[row].MacAddr] += 5;
+                }
+            }
+        }
+        
+        //读取收藏表
+    	db.find({
+    		"table":"WifiFollow",
+    	}, function(err, data) {
+    		if (data == null)
+    			return;
+    			
+    		var objects = JSON.parse(data);
+            for (var cursor in objects) {
+                var rows = objects[cursor];
+                for (var row in rows) {
+                    if (map_rank[rows[row].MacAddr] == null) {
+                        map_rank[rows[row].MacAddr] = 0;
+                    }
+                    map_rank[rows[row].MacAddr] += 5;
+                }
+            }
+            
+            //读取举报表
+        	db.find({
+        		"table":"WifiReport",
+        	}, function(err, data) {
+        		if (data == null)
+        			return;
+        			
+        		var objects = JSON.parse(data);
+                for (var cursor in objects) {
+                    var rows = objects[cursor];
+                    for (var row in rows) {
+                        if (map_rank[rows[row].MacAddr] == null) {
+                            map_rank[rows[row].MacAddr] = 0;
+                        }
+                        map_rank[rows[row].MacAddr] -= 5;
+                    }
+                }
+                
+                //插入数据到表
+                var index = 0;
+                for (var cursor in map_rank) {
+                    db.insert({
+                        "table":"WifiRank",             //表名
+                        "data":{"MacAddr" : cursor, "Score" : map_rank[cursor], "Rank" : ++index} //需要更新的数据，格式为JSON
+                    }, function(err, data) {         //回调函数
+                         if(err)  response.end("error is  " + err.code  + "error message is " + err.error );
+                    });
+                }
+                response.end(cursor + " = " + map_rank[cursor]);
+        	});
+    	});
 	});
-	
-	//读取收藏表
-	db.find({
-		"table":"WifiFollow",
-	}, function(err, data) {
-		if (data == null)
-			return;
-			
-		var rows = JSON.parse(data);
-		for (var cursor in rows) {
-            if (!map_rank.contains(cursor.MacAddr))
-                map_rank[cursor.Macaddr] = 0;
-            map_rank[cursor.Macaddr] += 5;
-		}
-	});
-	
-	//读取举报表
-	db.find({
-		"table":"WifiReport",
-	}, function(err, data) {
-		if (data == null)
-			return;
-			
-		var rows = JSON.parse(data);
-		for (var cursor in rows) {
-            if (!map_rank.contains(cursor.MacAddr))
-                map_rank[cursor.Macaddr] = 0;
-            map_rank[cursor.Macaddr] -= 5;
-		}
-	});
-
-    response.end(map_rank.toString());
-}
+}                                                                                                                                                                                                                                                
