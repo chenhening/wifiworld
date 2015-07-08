4b7d1d48fb4eb97034626114fc7330896e0ba5bd
binfeilx <binfeix.li@intel.com>
2015-07-08 12:00:48 +0800

    fixed bug

--- client/android/WifiWorld_mini/src/com/anynet/wifiworld/provider/WifiProviderSettingActivity.java
+++ client/android/WifiWorld_mini/src/com/anynet/wifiworld/provider/WifiProviderSettingActivity.java
@@ -1,7 +1,6 @@
 package com.anynet.wifiworld.provider;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
